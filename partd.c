b98f580ab6db100da508d2618a40d230debd2bd6
veevinn <41170192+veevinn@users.noreply.github.com>
2022-03-04 13:52:25 -0500

    Update partd.c

--- partd.c
+++ partd.c
@@ -5,63 +5,173 @@
  * Author : veevinn
  */ 
 
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <stdio.h>
 #define F_CPU 16000000UL
-#include <util/delay.h>
 #define BAUD_RATE 9600
 #define BAUD_PRESCALER (((F_CPU / (BAUD_RATE * 16UL))) - 1)
 
-
-/*--------------------Libraries---------------------------*/
+#include <avr/io.h>
+#include <avr/interrupt.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+#include <util/delay.h>
 #include "uart.h"
 
-void Initialize() {
-	
-	CLKPR = (1<<CLKPCE);
-	CLKPR |= (1<<CLKPS0);
-	
+char String[25];
+volatile int dist = 0;
+volatile int start = 0;
+volatile int end = 0;
+volatile int rising = 1;
+volatile int ocra = 956;
+volatile int cont_mode = 1;
+
+void Initialize(){
+		
 	cli();
-	
+
 	// set output pin
 	DDRD |= (1<<DDD6);
 	PORTD |= (1<<PORTD6);
 
-	// prescale clock by 8
+	// prescale timer0 by 8
 	TCCR0B &= ~(1<<CS00);
 	TCCR0B |= (1<<CS01);
 	TCCR0B &= ~(1<<CS02);
-	
-	// clock in normal PWM mode
-	TCCR0A &= ~(1<<WGM00);
+		
+	// clock in PWM mode
+	TCCR0A |= (1<<WGM00);
 	TCCR0A &= ~(1<<WGM01);
-	TCCR0B &= ~(1<<WGM02);
-	
-//	TCCR0A |= (1<<COM0B1);
-//	TCCR0A |= (1<<COM0A0);
-//	OCR0A = 30;
-//	OCR0B = 15;
+	TCCR0B |= (1<<WGM02);
+		
+	TCCR0A |= (1<<COM0A0);
+	TCCR0A |= (1<<COM0B1);
+		
+	// 440 Hz output capture
+	OCR0A = ocra;
+	OCR0B = ocra / 2;
 
-	TIMSK0 |= (1<<TOIE0);
-//	TIMSK0 |= (1<<OCIE0A);
-//	TIFR0 |= (1<<OCF0B);
-	TIFR0 |= (1<<TOV0);
-	sei();
+	TIMSK0 |= (1<<OCIE0A);
+	TIFR0 |= (1<<OCF0A);
+		
+	//GPIO PIN SETUP
+	//Ultrasound Sensor Echo Pin
+	DDRB &= ~(1<<DDB0);//Set Pin 8 (PB0) to be input pin
+	//Ultrasound Sensor Trig Pin
+	DDRD |= (1<<DDD7); //Set Pin 7 (PD7) to output
+
+	//10us INITIAL TRIGGER PULSE
+	PORTD |= (1<<PORTD7); //Toggle Pin 7 (D7) HIGH
+	_delay_us(10);
+	PORTD &= ~(1<<PORTD7); //Toggle Pin 7 (D7) LOW
+	
+	//TIMER1 ADJUSTMENTS
+	//Enable clock at 2MHz with (clkio/8 prescaling)
+	TCCR1B &= ~(1<<CS10); //0
+	TCCR1B |= (1<<CS11); //1
+	TCCR1B &= ~(1<<CS12); //0
+	
+	//Enable Normal mode for ICR1 register (WGM10 - 0, WGM11 - 0, WGM12 - 0, WGM13 - 0)
+	TCCR1A &= ~(1<<WGM10);
+	TCCR1A &= ~(1<<WGM11);
+	TCCR1B &= ~(1<<WGM12);
+	TCCR1B &= ~(1<<WGM13);
+	
+	TIMSK1 |= (1<<ICIE1); //Enable input capture interrupt
+	TCCR1B |= (1<<ICES1); //Look for rising edge
+	TIFR1 |= (1<<ICF1); //Clear interrupt flag
+	
+	//Enable overflow interrupt
+	TIMSK1 |= (1<<TOIE1);
+	TIFR1 |= (1<<TOV1);
+	
+	DDRB &= ~(1<<DDB1);
+	
+	
+	sei();//Enable global interrupts
 }
 
-ISR(TIMER0_OVF_vect) {
-	PORTD ^= (1<<PORTD6);
-	//TCNT0 = 0;
+//Input Capture Interrupt that measures the distance being outputted by the Ultrasonic Sensor
+ISR(TIMER1_CAPT_vect)
+{
+	if (rising == 1)
+	{
+		start = ICR1;
+		TCCR1B &= ~(1<<ICES1);
+		rising = 0;
+	}
+	else if (rising == 0)
+	{
+		end = ICR1;
+		// Calculate the distance in centimeters with calibration factor 0.5, calculation deals with cm and us
+		dist = ((end-start)*0.034/2)*0.5;
+		ocra = (dist)*1.724 + 477;				
+		TCCR1B |= (1<<ICES1);
+		rising = 1;
+	}
+}
+
+//Timer1 Overflow Interrupt that periodically sends a trigger signal to the Ultrasonic Sensor
+ISR(TIMER1_OVF_vect)
+{
+	//Output the distance to the serial monitor
+	if (dist >= 0 && dist <= 400)
+	{		
+		sprintf(String,"Distance: %u cm\n", dist);
+		UART_putstring(String);
+		dist = 0;
+	}
+	
+	//10us trigger pulse
+	PORTD |= (1<<PORTD7); //Toggle Pin 7 (D7) HIGH
+	_delay_us(10);
+	PORTD &= ~(1<<PORTD7); //Toggle Pin 7 (D7) LOW
+	start = 0;
+	end = 0;
+}
+
+ISR(TIMER0_COMPA_vect){
+	if(PINB && (1<<PINB1)){
+		if(cont_mode == 1) {
+			cont_mode = 0;
+		}
+		else {
+			cont_mode = 1;
+		}
+	}
+	if(cont_mode == 1){
+		OCR0A = ocra;
+	}
+	else {
+		if(dist > 0 && dist <= 33) {
+			OCR0A = 477;
+		}
+		if(dist > 33 && dist <= 66) {
+			OCR0A = 477;
+		}
+		if(dist > 66 && dist <= 100) {
+			OCR0A = 477;
+		}
+		if(dist > 100 && dist <= 133) {
+			OCR0A = 477;
+		}
+		if(dist > 133 && dist <= 166) {
+			OCR0A = 477;
+		}
+		if(dist > 166 && dist <= 200) {
+			OCR0A = 477;
+		}
+		if(dist > 200 && dist <= 233) {
+			OCR0A = 477;
+		}
+		if(dist > 233 && dist <= 267) {
+			OCR0A = 477;
+		}
+	}
+	OCR0B = OCR0A / 2;
 }
 
 int main(void)
 {
-    Initialize();
-    while (1);
+	Initialize();
+	UART_init(BAUD_PRESCALER);
+	while(1);
 }
-
-
