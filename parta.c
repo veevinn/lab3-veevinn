3a2368311f0eb31854595703a83fa5da46a8cd04
veevinn <41170192+veevinn@users.noreply.github.com>
2022-03-05 16:07:50 -0500

    Update parta.c

--- parta.c
+++ parta.c
@@ -1,1 +1,26 @@
+#include "parta.h"
+#include <avr/io.h>
 
+void parta_init(int prescalar)
+{
+  
+  /*Set baud rate */
+  UBRR0H = (unsigned char)(prescalar>>8);
+  UBRR0L = (unsigned char)prescalar;
+  //Enable receiver and transmitter 
+  UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+
+  UCSR0C = (1<<UCSZ01) | (1<<UCSZ00); // 8 data bits
+  UCSR0C |= (1<<USBS0); // 2 stop bits
+}
+
+void parta_output(char* strpt)
+{
+  while(*strpt != 0x00)
+  {
+    while(!(UCSR0A & (1<<UDRE0)));  
+    // Put data into buffer and send data
+    UDR0 = *strpt;
+    strpt++;
+    }
+}
