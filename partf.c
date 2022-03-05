fb7deaf2ff924357b6cfa0d59df4a6c05f01feb8
veevinn <41170192+veevinn@users.noreply.github.com>
2022-03-05 15:49:51 -0500

    Update partf.c

--- partf.c
+++ partf.c
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <util/delay.h>
-#include "uart.h"
+#include "serialparta.h"
 
 char String[25];
 volatile int dist = 0;
@@ -15,12 +15,12 @@ volatile int start = 0;
 volatile int end = 0;
 volatile int rising = 1;
 volatile int ocra = 956;
-volatile float ocrb =  0.5;
+volatile float ocrb =  50;
 volatile int cont_mode = 1;
 
 void Initialize()
 {
-	cli();//disable global interrupts
+	cli();
 	
 	// set output pin
 	DDRD |= (1<<DDD6);
@@ -31,7 +31,7 @@ void Initialize()
 	TCCR0B |= (1<<CS01);
 	TCCR0B &= ~(1<<CS02);
 	
-	// clock in PWM mode
+	// timer0 in PWM mode
 	TCCR0A |= (1<<WGM00);
 	TCCR0A &= ~(1<<WGM01);
 	TCCR0B |= (1<<WGM02);
@@ -39,84 +39,78 @@ void Initialize()
 	TCCR0A |= (1<<COM0A0);
 	TCCR0A |= (1<<COM0B1);
 	
-	// 440 Hz output capture
+	// output capture
 	OCR0A = ocra;
-	OCR0B = ocra * ocrb;
+	OCR0B = ocra * (ocrb / 100);
 
 	TIMSK0 |= (1<<OCIE0A);
 	TIMSK0 |= (1<<OCIE0B);
 	TIFR0 |= (1<<OCF0A);
 	
-	//GPIO PIN SETUP
 	//Ultrasound Sensor Echo Pin
-	DDRB &= ~(1<<DDB0);//Set Pin 8 (PB0) to be input pin
+	DDRB &= ~(1<<DDB0);
 	//Ultrasound Sensor Trig Pin
-	DDRD |= (1<<DDD7); //Set Pin 7 (PD7) to output
+	DDRD |= (1<<DDD5); 
 
-	//10us INITIAL TRIGGER PULSE
-	PORTD |= (1<<PORTD7); //Toggle Pin 7 (D7) HIGH
+	//10us initial trigger
+	PORTD |= (1<<PORTD5); 
 	_delay_us(10);
-	PORTD &= ~(1<<PORTD7); //Toggle Pin 7 (D7) LOW
+	PORTD &= ~(1<<PORTD5); 
 	
-	//TIMER1 ADJUSTMENTS
-	//Enable clock at 2MHz with (clkio/8 prescaling)
+	//prescale timer1 by 8
 	TCCR1B &= ~(1<<CS10); //0
 	TCCR1B |= (1<<CS11); //1
 	TCCR1B &= ~(1<<CS12); //0
 	
-	//Enable Normal mode for ICR1 register (WGM10 - 0, WGM11 - 0, WGM12 - 0, WGM13 - 0)
+	//timer1 in normal mode
 	TCCR1A &= ~(1<<WGM10);
 	TCCR1A &= ~(1<<WGM11);
 	TCCR1B &= ~(1<<WGM12);
 	TCCR1B &= ~(1<<WGM13);
 	
-	TIMSK1 |= (1<<ICIE1); //Enable input capture interrupt
-	TCCR1B |= (1<<ICES1); //Look for rising edge
-	TIFR1 |= (1<<ICF1); //Clear interrupt flag
+	// input capture
+	TIMSK1 |= (1<<ICIE1); 
+	TCCR1B |= (1<<ICES1); 
+	TIFR1 |= (1<<ICF1); 
 	
-	//Enable overflow interrupt
+	//overflow interrupt
 	TIMSK1 |= (1<<TOIE1);
 	TIFR1 |= (1<<TOV1);
 	
+	// input capture button pin
 	DDRB &= ~(1<<DDB1);
 	PCICR |= (1<<PCIE0);
 	PCMSK0 |= (1<<PCINT1);
 	
 	//ADC Set Up
-	//Disable ADC Power Reduction
 	PRR &= ~(1<<PRADC);
 	
 	//Select the reference voltage as AVcc
 	ADMUX |= (1<<REFS0);
 	ADMUX &= ~(1<<REFS1);
 	
-	//Set the ADC Clock prescaler to 16M/128 = 125kHz (clock must be between 50 and 200kHz)
+	//prescale by 128
 	ADCSRA |= (1<<ADPS0);
 	ADCSRA |= (1<<ADPS1);
 	ADCSRA |= (1<<ADPS2);
 	
-	//Select Pin A0 channel to read
 	ADMUX &= ~(1<<MUX0);
 	ADMUX &= ~(1<<MUX1);
 	ADMUX &= ~(1<<MUX2);
 	ADMUX &= ~(1<<MUX3);
 	
-	//Set ADC to be free running
 	ADCSRA |= (1<<ADATE);
 	ADCSRB &= ~(1<<ADTS0);
 	ADCSRB &= ~(1<<ADTS1);
 	ADCSRB &= ~(1<<ADTS2);
 	
-	//Disable input buffer
 	DIDR0 |= (1<<ADC0D);
 	
-	//Enable ADC
 	ADCSRA |= (1<<ADEN);
 	
-	//Start conversion
 	ADCSRA |= (1<<ADSC);
 	
-	sei();//Enable global interrupts
+	sei();
 }
 
 ISR(PCINT0_vect) {
@@ -130,7 +124,6 @@ ISR(PCINT0_vect) {
 	}
 }
 
-//Input Capture Interrupt that measures the distance being outputted by the Ultrasonic Sensor
 ISR(TIMER1_CAPT_vect)
 {
 	if (rising == 1)
@@ -142,12 +135,14 @@ ISR(TIMER1_CAPT_vect)
 	else if (rising == 0)
 	{
 		end = ICR1;
-		// Calculate the distance in centimeters with calibration factor 0.5, calculation deals with cm and us
+		// Calculate distance in cm
 		dist = ((end-start)*0.034/2)*0.5;
 		if(cont_mode == 1){
+			// find frequency from equation
 			ocra = (dist)*1.724 + 477;
 		}
 		else {
+			//find frequency in discrete mode
 			if(dist > 0 && dist <= 33) {
 				ocra = 477;
 			}
@@ -179,78 +174,78 @@ ISR(TIMER1_CAPT_vect)
 	}
 }
 
-//Timer1 Overflow Interrupt that periodically sends a trigger signal to the Ultrasonic Sensor
 ISR(TIMER1_OVF_vect)
 {
 	//Output the distance to the serial monitor
 	if (dist >= 0 && dist <= 400)
 	{
 		sprintf(String,"Distance: %u cm\n", dist);
-		UART_putstring(String);
+		serialparta_output(String);
 		dist = 0;
 	}
 	
 	//10us trigger pulse
-	PORTD |= (1<<PORTD7); //Toggle Pin 7 (D7) HIGH
+	PORTD |= (1<<PORTD7); 
 	_delay_us(10);
-	PORTD &= ~(1<<PORTD7); //Toggle Pin 7 (D7) LOW
+	PORTD &= ~(1<<PORTD7); 
 	start = 0;
 	end = 0;
 }
 
 ISR(TIMER0_COMPA_vect){
-	
+	// set frequency
 	OCR0A = ocra;
 	
 }
 
 ISR(TIMER0_COMPB_vect) {
+	// set duty cycle
 	if (ADC >= 2 && ADC < 103)
 	{
-		ocrb = 0.5;
+		ocrb = 5;
 	}
 	else if(ADC >= 104 && ADC < 205)
 	{
-		ocrb = 0.10;
+		ocrb = 10;
 	}
 	else if(ADC >= 206 && ADC < 307)
 	{
-		ocrb = 0.15;
+		ocrb = 15;
 	}
 	else if(ADC >= 308 && ADC < 409)
 	{
-		ocrb = 0.20;
+		ocrb = 20;
 	}
 	else if(ADC >= 410 && ADC < 511)
 	{
-		ocrb = 0.25;
+		ocrb = 25;
 	}
 	else if(ADC >= 512 && ADC < 613)
 	{
-		ocrb = 0.30;
+		ocrb = 30;
 	}
 	else if(ADC >= 614 && ADC < 715)
 	{
-		ocrb = 0.35;
+		ocrb = 35;
 	}
 	else if(ADC >= 716 && ADC < 817)
 	{
-		ocrb = 0.40;
+		ocrb = 40;
 	}
 	else if(ADC >= 818 && ADC < 919)
 	{
-		ocrb = 0.45;
+		ocrb = 45;
 	}
 	else if(ADC >= 920 && ADC < 1022)
 	{
-		ocrb = 0.50;
+		ocrb = 50;
 	}
-	OCR0B = ocra * ocrb;
+	OCR0B = ocra * (ocrb / 100);
 }
 
 int main(void)
 {
 	Initialize();
-	UART_init(BAUD_PRESCALER);
+	serialparta_init(BAUD_PRESCALER);
 	while(1);
 }
